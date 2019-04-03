7c7883384e21d25290c05c409304405dac7babcf
jfox <FoxJonathanP@gmail.com>
2019-04-03 10:44:40 -0700

    Added price and changed colors in ListingsCard

--- sf-housing-vp/src/components/ListingsDisplay/ListingCard/index.js
+++ sf-housing-vp/src/components/ListingsDisplay/ListingCard/index.js
@@ -1,22 +1,29 @@
 import React, { Component, } from 'react';
-import { Button, } from "reactstrap";
 
 class ListingCard extends Component {
 
   render() {
     return (
-      <div class="d-flex flex-row" style={{margin:"1rem 0", height:"12rem",}}>
+      <div class="d-flex flex-row bg-light" style={{margin:"1rem 0", height:"12rem", 
+        border:"1px solid var(--muted)", borderRadius:"4px"}}>
 
+        {/* Image */}
         <img src={this.props.src} alt={this.props.title}
           style={{objectFit:"cover", width:"20rem", height:"100%", borderRadius:"4px"}}/>
+
+        {/* Spacer */}
         <div style={{width:"1rem"}} />
 
-        <div class="d-flex flex-column">
-          <div class="p-2 flex-grow-1">
+        {/* Info */}
+        <div class="d-flex flex-column" style={{height:"100%"}}>
+          <div class="p-2 flex-grow-2">
             <div style={{height:"0.5rem"}} />
             <p class="text-secondary" style={{lineHeight:"0.8rem", textTransform:"uppercase"}}>{this.props.type}</p>
             <p style={{lineHeight:"0.8rem", fontWeight:"bold", fontSize:"1.3rem"}}>{this.props.title}</p>
-            <p >{this.props.description}</p>
+            <p>{this.props.description}</p>
+          </div>
+          <div class="p-2">
+            <p style={{fontSize:"1.3rem"}}>${this.props.price.toFixed(2)}</p>
           </div>
         </div>
 
