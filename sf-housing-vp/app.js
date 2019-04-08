45e6249c3cdee78ab45800a1800d8cb0544a5246
Hashim Jacobs <hashjaco@github.com>
2019-04-08 12:18:25 -0700

    routed root to index.html and modified environment to standard

--- sf-housing-vp/app.js
+++ sf-housing-vp/app.js
@@ -1,4 +1,4 @@
-const db = require('./database/queries');
+const db = require('./server/database/queries');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,14 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use('/assets/', express.static('./assets/'));
+
+/* Routes */
+
 app.get('/listings', db.getListings);
 app.get('/listings/:id', db.getListingById);
 app.post('/listings', db.createListing);
 app.put('/listings/:id', db.updateListing);
 app.delete('/listings/:id', db.deleteListing);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}!`);
-});
+app.listen(PORT);
 
 module.exports = app;
--- sf-housing-vp/server/database/queries.js
+++ sf-housing-vp/server/database/queries.js
@@ -13,36 +13,42 @@ const getListings = (req, res) => {
         if (error){
             throw error;
         }
-        response.status(200).json(results.rows);
+        res.status(200).json(results.rows);
     });
 };
 
 /* Return listings by id */
 const getListingById = (req, res) => {
-    const id = parseInt(request.params.listing_id);
+    const id = parseInt(req.params.listing_id);
 
     pool.query('SELECT * FROM listings WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error;
         }
-        response.status(200).json(results.rows);
+        res.status(200).json(results.rows);
     });
 };
 
 const createListing = (req, res) => {
-    const { id, type, price, image_path } = request.body;
+    const { type, price, image_path } = req.body;
 
-    pool.query ('INSERT INTO listings (id, type, price, image_path) VALUES ($1, $2, $3, $4)', [id, type, price, image_path], (error, results) => {
+    pool.query ('INSERT INTO listings (type, price, image_path) VALUES ($1, $2, $3, $4)', [type, price, image_path], (error, results) => {
         if (error){
             throw error;
         }
-        response.status(201).send('Listing added with ID: ${result.insertId}');
+        res.status(201).send('Listing added with ID: ${result.insertId}');
     });
 };
 
-const updateListing = (request, response) => {
-    const id = parseInt(request.params.listing_id);
-    const { type, price, image_path } = request.body;
+const searchListings = (req, res) => {
+    const search = req.body;
+
+    pool.query(`SELECT to_tsvector(${search});`);
+}
+
+const updateListing = (req, res) => {
+    const id = parseInt(req.params.listing_id);
+    const { type, price, image_path } = req.body;
 
     pool.query(
         'UPDATE listings SET type = $1, price = $2 WHERE id = $3',
@@ -51,19 +57,19 @@ const updateListing = (request, response) => {
             if (error) {
                 throw error;
             }
-            response.status(200).send(`Listing modified with ID: ${id}`);
+            res.status(200).send(`Listing modified with ID: ${id}`);
         }
     );
 };
 
-const deleteListing = (request, response) => {
-    const id = parseInt(request.params.listing_id);
+const deleteListing = (req, res) => {
+    const id = parseInt(req.params.listing_id);
 
     pool.query('DELETE FROM listings WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error;
         }
-        response.status(200).send(`Listing deleted with ID: ${id}`);
+        res.status(200).send(`Listing deleted with ID: ${id}`);
     });
 };
 
@@ -71,6 +77,7 @@ module.exports = {
     getListings,
     getListingById,
     createListing,
+    searchListings,
     updateListing,
     deleteListing
 };
