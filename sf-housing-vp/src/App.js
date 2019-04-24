534336d4629e7bc42cfd5d25e15f6620e3a887a2
Shalaka Aigal <shalaka.aigal@gmail.com>
2019-04-23 18:38:03 -0700

    Pass Text inserted to update listing

--- sf-housing-vp/src/App.js
+++ sf-housing-vp/src/App.js
@@ -7,29 +7,40 @@ import { Form, FormGroup, Label, Input, Button, } from 'reactstrap';
 class App extends Component {
 	constructor(props) {
     super(props);
-    this.state = {searchClicked: false};
+    this.state = {
+      searchClicked: false,
+      topicBox: null,
+      searchWord: "",
+    };
 
     // This binding is necessary to make `this` work in the callback
     this.clickSearch = this.clickSearch.bind(this);
     this.renderListings = this.renderListings.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
    clickSearch() {
+    console.log( this.state.topicBox); 
     this.setState({searchClicked: true });
   }
 
   renderListings(){
     var listingToDisplay;
     if (this.state.searchClicked) {
-      listingToDisplay = <ListingsDisplay searchType= '/searchproperties/Room' />;
+      listingToDisplay = <ListingsDisplay searchType= {"/searchproperties/"+this.state.topicBox}  />;
     } else {
       listingToDisplay = <ListingsDisplay searchType= '/properties' />;
     }
 
     return(
           <div>
-            {listingToDisplay};
+            {listingToDisplay}
           </div>
       );
   }
@@ -55,7 +66,8 @@ class App extends Component {
         <Form>
           <FormGroup>
             <Label for="searchText">Search</Label>
-            <Input type="text" id="searchBar" placeholder='e.g. "Studio near Stonestown"' />
+            <Input type="text" name="topicBox"  id="searchBar" placeholder='e.g. "Studio near Stonestown"' value={this.state.topicBox }
+              onChange={ this.handleChange } />
           </FormGroup>
 
           <FormGroup>
@@ -71,7 +83,7 @@ class App extends Component {
           {/* Spacer */}
           <div style={{height:"1rem"}}/>
 
-          <Button color="primary" style={{width:"100%", height:"3rem"}} onClick={this.clickSearch.bind(this)}>Search</Button>
+          <Button color="primary" style={{width:"100%", height:"3rem"}} onClick={this.clickSearch}>Search</Button>
         </Form>
 
         {/* Spacer */}
