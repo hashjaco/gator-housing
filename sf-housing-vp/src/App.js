3664041e38e025f03a0df250e6b4ae730cec5ffb
Shalaka Aigal <shalaka.aigal@gmail.com>
2019-04-23 17:24:41 -0700

    Frontend Search Functionality

--- sf-housing-vp/src/App.js
+++ sf-housing-vp/src/App.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from './routes/Home'
+import ListingsDisplay from './components/ListingsDisplay';
+import { Form, FormGroup, Label, Input, Button, } from 'reactstrap';
+
 
 class App extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {searchClicked: false};
+
+    // This binding is necessary to make `this` work in the callback
+    this.clickSearch = this.clickSearch.bind(this);
+    this.renderListings = this.renderListings.bind(this);
+  }
+
+  
+   clickSearch() {
+    this.setState({searchClicked: true });
+  }
+
+  renderListings(){
+    var listingToDisplay;
+    if (this.state.searchClicked) {
+      listingToDisplay = <ListingsDisplay searchType= '/searchproperties/Room' />;
+    } else {
+      listingToDisplay = <ListingsDisplay searchType= '/properties' />;
+    }
+
+    return(
+          <div>
+            {listingToDisplay};
+          </div>
+      );
+  }
 
   render() {
     return (
-      <div style={{maxWidth:'800px', padding:'1rem', margin:'auto'}}>
-        <Home/>
+      // Main Div
+      <div style={{maxWidth:"60rem", margin:"auto"}}>
+
+        {/* Spacer */}
+        <div style={{height:"3rem"}}/>
+
+        {/* Title */}
+        <div style={{textAlign:"center"}}>
+          <h1>SFSU Housing (Vertical Prototype)</h1>
+          <h2>CSC 648 Team 14</h2>
+        </div>
+
+        {/* Spacer */}
+        <div style={{height:"2rem"}}/>
+
+        {/* Search Form */}
+        <Form>
+          <FormGroup>
+            <Label for="searchText">Search</Label>
+            <Input type="text" id="searchBar" placeholder='e.g. "Studio near Stonestown"' />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="roomType">Listing Type</Label>
+            <Input type="select" id="roomType" >
+              <option>Any</option>
+              <option>Room</option>
+              <option>Studio</option>
+              <option>House</option>
+            </Input>
+          </FormGroup>
+
+          {/* Spacer */}
+          <div style={{height:"1rem"}}/>
+
+          <Button color="primary" style={{width:"100%", height:"3rem"}} onClick={this.clickSearch.bind(this)}>Search</Button>
+        </Form>
+
+        {/* Spacer */}
+        <div style={{height:"1rem"}}/>
+
+        {/* The listings are here */}
+        <div>
+        {this.renderListings()}
+        </div>
+        
       </div>
     );
   }
--- sf-housing-vp/src/components/ListingsDisplay/index.js
+++ sf-housing-vp/src/components/ListingsDisplay/index.js
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
 import ListingCard from "./ListingCard";
 
-
-
-
 class ListingsDisplay extends Component {
+	constructor(props) {
+        super(props)
+        this.state = {
+          type : this.props.searchType,
+            properties: []
+        }
+        this.updateListing = this.updateListing.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateListing();
+    }
+
+    componentDidUpdate(prevProps){
+      if(JSON.stringify(this.props.searchType) !== JSON.stringify(prevProps.searchType))
+      {
+            this.updateListing();
+      }
+    }
+
+    updateListing(){
+      let self = this;
+        fetch(this.props.searchType, {
+            method: 'GET'
+        }).then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        }).then(function(data) {
+            self.setState({properties: data});
+        }).catch(err => {
+        console.log('caught it!',err);
+        })
+    }
+
+
 
   render() {
-    if (!this.props.data) {
-      return
+
+    if (this.state.properties) {
+      console.log(JSON.stringify(this.state.properties))
     }
 
     return (
       <div class="d-flex flex-column">
-        {this.props.data.map(member =>
+        {this.state.properties.map(member =>
  
                    
         <ListingCard title="A House" type={member.property_type} src="https://media.gettyimages.com/photos/traditional-suburban-house-picture-id103761767?s=612x612"
