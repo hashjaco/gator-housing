37c0e6ff98e9501b402cea24c7411a058e63ad99
Hashim Jacobs <hashjaco@github.com>
2019-04-28 05:03:56 -0700

    Implemented User SQL queries(partial)

--- sf-housing-vp/server/database/queries.js
+++ sf-housing-vp/server/database/queries.js
@@ -7,6 +7,8 @@ const pool = new Pool({
     port: 5432,
 });
 
+/* PROPERTY QUERIES */
+
 /* Return all properties */
 const getProperties = (req, res) => {
     pool.query('SELECT * FROM properties ORDER BY id ASC', (error, results) => {
@@ -42,27 +44,17 @@ const createProperty = (req, res) => {
 
 const searchProperties = (req, res) => {
     const search = '%'+req.params.key+'%';
-
-    pool.query(`SELECT * FROM properties 
-    WHERE address || ' ' || property_type || ' ' || zip_code || ' ' || price  ILIKE  ($1)`,[search], (error, results) => {
-        if (error){
-            throw error;
-        }
-        res.status(200).json(results.rows);
+    const searchArray = search.split(" ");
+    searchArray.forEach(string => {
+      pool.query(`SELECT * FROM properties
+      WHERE address || ' ' || property_type || ' ' || zip_code || ' ' || price  ILIKE  ($1)`,[search], (error, results) => {
+          if (error){
+              throw error;
+          }
+          res.status(200).json(results.rows);
+      });
     });
-    
 }
-
-const searchPropertiesByListings = (req, res) => {
-	    const type = parseInt(req.params.property_type);
-
-    pool.query('SELECT * FROM properties WHERE property_type = $1', [t], (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).json(results.rows);
-    });
-};
 
 const updateProperty = (req, res) => {
     const id = parseInt(req.params.properties_id);
@@ -90,6 +82,94 @@ const deleteProperty = (req, res) => {
         res.status(200).send(`Property deleted with ID: ${id}`);
     });
 };
+
+
+/* USERS QUERIES */
+
+/* Return all users */
+const getAllUsers = (req, res) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error){
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+};
+
+/* Return users by id */
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+};
+
+const addUser = (req, res) => {
+    const { type, price, image_path } = req.body;
+
+    pool.query ('INSERT INTO users (User_type, price, image_path) VALUES ($1, $2, $3)', [type, price, image_path], (error, results) => {
+        if (error){
+            throw error;
+        }
+        res.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+};
+
+const searchUsers = (req, res) => {
+    const search = '%'+req.params.key+'%';
+
+    pool.query(`SELECT * FROM users
+    WHERE address || ' ' || user_name || ' ' || phone_number || ' ' ||  ILIKE  ($1)`,[search], (error, results) => {
+        if (error){
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+
+}
+
+const searchUsersByListings = (req, res) => {
+	    const type = parseInt(req.params.User_type);
+
+    pool.query('SELECT * FROM users WHERE User_type = $1', [t], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+};
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.users_id);
+    const { type, price, image_path } = req.body;
+
+    pool.query(
+        'UPDATE users SET type = $1, price = $2 WHERE id = $3',
+        [type, price, id],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).send(`User modified with ID: ${id}`);
+        }
+    );
+};
+
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.User_id);
+
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).send(`User deleted with ID: ${id}`);
+    });
+};
+
 
 module.exports = {
     getProperties,
