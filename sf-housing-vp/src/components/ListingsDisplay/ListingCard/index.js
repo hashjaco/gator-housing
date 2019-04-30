a89fdd25d14c3e47a0c93d7314346412a569a16e
jfox <FoxJonathanP@gmail.com>
2019-04-30 16:17:50 -0700

    Added Jarrett's modal and added map to modal

--- sf-housing-vp/src/components/ListingsDisplay/ListingCard/index.js
+++ sf-housing-vp/src/components/ListingsDisplay/ListingCard/index.js
@@ -1,9 +1,35 @@
 import './index.css'
 import React, { Component, } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import GoogleMapReact from 'google-map-react';
+
+const MAPS_API_KEY = "AIzaSyBllbHD-lG5no2m1IFdtdckhDETM4n4dw4"
 
 
 class ListingCard extends Component {
+
+  static defaultProps = {
+    center: {
+      lat: 37.721558,
+      lng: -122.478165,
+    },
+    zoom: 11
+  };
+
+  constructor(props){ 
+    super(props);
+    this.state = {
+      modal: false,
+      ...props
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle(){
+    this.setState((prevState) => {
+      return {modal: !prevState.modal}
+    })
+  }
 
   toPriceString = (number) => {
     return "$" + number.toLocaleString(undefined, {maximumFractionDigits:2, minimumFractionDigits:2});
@@ -11,18 +37,15 @@ class ListingCard extends Component {
 
   render() {
     return (
-      <Link className="listingcard d-flex flex-row" to="/desc">
+      <Link className="listingcard d-flex flex-row" onClick={this.toggle}>
 
         {/* Image */}
-        <div>
+        <div style={{marginRight:"1rem"}}>
           <img className="listingcard-image" src={this.props.src} alt={this.props.title}/>
           <div className="listingcard-image-text">
             <p>{this.toPriceString(this.props.price)}</p>
           </div>
         </div>
-
-        {/* Spacer */}
-        <div style={{width:"1rem"}} />
 
         {/* Info */}
         <div class="d-flex flex-column">
@@ -33,6 +56,28 @@ class ListingCard extends Component {
             <p>{this.props.description}</p>
           </div>
         </div>
+
+        {/* This is the detailed view that comes up when a ListingCard is clicked. */}
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
+          <ModalBody>
+            <img src={this.props.src} alt={this.props.title} style={{width:"100%", height:"16rem", objectFit:"cover"}}/>
+            <p> {this.state.description} </p>
+            <p> {this.toPriceString(this.state.price)} / month</p>
+            {/* Map */}
+            <div style={{height:'16rem', width:'100%'}}>
+              <GoogleMapReact
+                bootstrapURLKeys={{key: MAPS_API_KEY}}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom} />
+            </div>
+            
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Message Landlord</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Back to Listings</Button>
+          </ModalFooter>
+        </Modal>
 
       </Link>
     )
